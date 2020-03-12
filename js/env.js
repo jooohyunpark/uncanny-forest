c481419573b17a4258aba4ed92ad50ea7eb0174b
Joohyun Park <jhp527@nyu.edu>
2020-03-12 11:54:48 -0400

    update style wip

--- js/env.js
+++ js/env.js
@@ -27,7 +27,6 @@ AFRAME.registerComponent('environment', {
             min: 0,
             max: 1000
         },
-
         skyType: {
             default: 'color',
             oneOf: ['none', 'color', 'gradient', 'atmosphere']
@@ -72,7 +71,6 @@ AFRAME.registerComponent('environment', {
             min: 0.5,
             max: 10
         },
-
         ground: {
             default: 'hills',
             oneOf: ['none', 'flat', 'hills', 'canyon', 'spikes', 'noise']
@@ -95,7 +93,6 @@ AFRAME.registerComponent('environment', {
             type: 'color',
             default: '#694439'
         },
-
         dressing: {
             default: 'mushrooms',
             oneOf: ['none', 'cubes', 'pyramids', 'cylinders', 'hexagons', 'stones', 'trees', 'mushrooms', 'towers', 'apparatus', 'arches', 'torii']
@@ -133,7 +130,6 @@ AFRAME.registerComponent('environment', {
             min: 0,
             max: 1
         },
-
         grid: {
             default: 'none',
             oneOf: ['none', '1x1', '2x2', 'crosses', 'dots', 'xlines', 'ylines']
@@ -147,16 +143,15 @@ AFRAME.registerComponent('environment', {
     multiple: false,
 
     presets: {
-        'none': {},
         'forest': {
             active: true,
             seed: 1,
             skyType: 'atmosphere',
-            skyColor: '#5200ff',
-            horizonColor: '#ccc',
+            skyColor: '#000',//'#5200ff',
+            horizonColor: '#000',//'#ccc',
             lighting: 'distant',
             lightPosition: {
-                 x: 0,
+                x: 0,
                 y: 0.02,
                 z: -0.46
             },
@@ -165,17 +160,17 @@ AFRAME.registerComponent('environment', {
             playArea: 1,
             ground: 'hills',
             groundYScale: 4,
-            groundTexture: 'walkernoise',
-            groundColor: '#FB61BF',
-            groundColor2: '#00ffff',            
-            dressing: 'trees',
+            groundTexture: 'none', //'walkernoise',
+            groundColor: '#000',//'#FB61BF',
+            groundColor2: '#000',//'#00ffff',
+            dressing: 'cylinders',
             dressingAmount: 2222,
-            dressingColor: '#00ffff', //00ffff
-            dressingScale: 1,
+            dressingColor: '#000',//'#00ffff', //00ffff
+            dressingScale: 0.1,
             dressingVariance: {
-                x: 1,
-                y: 22,
-                z: 1
+                x: 0.1,
+                y: 100,
+                z: 0.1
             },
             dressingUniformScale: true,
             dressingOnPlayArea: 0,
@@ -208,7 +203,7 @@ AFRAME.registerComponent('environment', {
                     segments: 10,
                     vertices: [0.000001, 0.768, 0.099, 0.772, 0.219, 0.802, 0.306, 0.844, 0.352, 0.886, 0.352, 0.908, 0.118, 0.904, 0.107, 0.93, 0.115, 0.966, 0.14, 0.996]
                 }
-      ],
+            ],
             'trees': [
                 {
                     type: 'lathe',
@@ -228,13 +223,13 @@ AFRAME.registerComponent('environment', {
                     segments: 14,
                     vertices: [0.000001, 0.436, 0.126, 0.46, 0.201, 0.57, 0.219, 0.72, 0.154, 0.846, 0.028, 0.884, 0.034, 0.996]
                 }
-      ] ,
-                        'hexagons': [
+            ],
+            'hexagons': [
                 {
                     type: 'extrude',
                     vertices: [-0.198, -0.302, 0.197, -0.3, 0.372, 0, 0.199, 0.298, -0.202, 0.298, -0.368, 0]
                 }
-      ]
+            ]
         };
 
         // scale down dressing meshes (coordinates were saved in integers for better compression)
@@ -1179,179 +1174,179 @@ AFRAME.registerShader('skyshader', {
     },
 
     vertexShader: [
-    'varying vec3 vWorldPosition;',
-    'void main() {',
-    'vec4 worldPosition = modelMatrix * vec4( position, 1.0 );',
-    'vWorldPosition = worldPosition.xyz;',
-    'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-    '}'
-  ].join('\n'),
+        'varying vec3 vWorldPosition;',
+        'void main() {',
+        'vec4 worldPosition = modelMatrix * vec4( position, 1.0 );',
+        'vWorldPosition = worldPosition.xyz;',
+        'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+        '}'
+    ].join('\n'),
 
     fragmentShader: [
-    'uniform sampler2D skySampler;',
-    'uniform vec3 sunPosition;',
-    'varying vec3 vWorldPosition;',
+        'uniform sampler2D skySampler;',
+        'uniform vec3 sunPosition;',
+        'varying vec3 vWorldPosition;',
 
-    'vec3 cameraPos = vec3(0., 0., 0.);',
+        'vec3 cameraPos = vec3(0., 0., 0.);',
 
-    'uniform float luminance;',
-    'uniform float turbidity;',
-    'uniform float reileigh;',
-    'uniform float mieCoefficient;',
-    'uniform float mieDirectionalG;',
+        'uniform float luminance;',
+        'uniform float turbidity;',
+        'uniform float reileigh;',
+        'uniform float mieCoefficient;',
+        'uniform float mieDirectionalG;',
 
-    // constants for atmospheric scattering'
-    'const float e = 2.71828182845904523536028747135266249775724709369995957;',
-    'const float pi = 3.141592653589793238462643383279502884197169;',
+        // constants for atmospheric scattering'
+        'const float e = 2.71828182845904523536028747135266249775724709369995957;',
+        'const float pi = 3.141592653589793238462643383279502884197169;',
 
-    // refractive index of air
-    'const float n = 1.0003;',
-    // number of molecules per unit volume for air at'
-    'const float N = 2.545E25;',
-    // 288.15K and 1013mb (sea level -45 celsius)
-    // depolatization factor for standard air
-    'const float pn = 0.035;',
-    // wavelength of used primaries, according to preetham
-    'const vec3 lambda = vec3(680E-9, 550E-9, 450E-9);',
+        // refractive index of air
+        'const float n = 1.0003;',
+        // number of molecules per unit volume for air at'
+        'const float N = 2.545E25;',
+        // 288.15K and 1013mb (sea level -45 celsius)
+        // depolatization factor for standard air
+        'const float pn = 0.035;',
+        // wavelength of used primaries, according to preetham
+        'const vec3 lambda = vec3(680E-9, 550E-9, 450E-9);',
 
-    // mie stuff
-    // K coefficient for the primaries
-    'const vec3 K = vec3(0.686, 0.678, 0.666);',
-    'const float v = 4.0;',
+        // mie stuff
+        // K coefficient for the primaries
+        'const vec3 K = vec3(0.686, 0.678, 0.666);',
+        'const float v = 4.0;',
 
-    // optical length at zenith for molecules
-    'const float rayleighZenithLength = 8.4E3;',
-    'const float mieZenithLength = 1.25E3;',
-    'const vec3 up = vec3(0.0, 1.0, 0.0);',
+        // optical length at zenith for molecules
+        'const float rayleighZenithLength = 8.4E3;',
+        'const float mieZenithLength = 1.25E3;',
+        'const vec3 up = vec3(0.0, 1.0, 0.0);',
 
-    'const float EE = 1000.0;',
-    'const float sunAngularDiameterCos = 0.999956676946448443553574619906976478926848692873900859324;',
-    // 66 arc seconds -> degrees, and the cosine of that
+        'const float EE = 1000.0;',
+        'const float sunAngularDiameterCos = 0.999956676946448443553574619906976478926848692873900859324;',
+        // 66 arc seconds -> degrees, and the cosine of that
 
-    // earth shadow hack'
-    'const float cutoffAngle = pi/1.95;',
-    'const float steepness = 1.5;',
+        // earth shadow hack'
+        'const float cutoffAngle = pi/1.95;',
+        'const float steepness = 1.5;',
 
-    'vec3 totalRayleigh(vec3 lambda)',
-    '{',
-    'return (8.0 * pow(pi, 3.0) * pow(pow(n, 2.0) - 1.0, 2.0) * (6.0 + 3.0 * pn)) / (3.0 * N * pow(lambda, vec3(4.0)) * (6.0 - 7.0 * pn));',
-    '}',
+        'vec3 totalRayleigh(vec3 lambda)',
+        '{',
+        'return (8.0 * pow(pi, 3.0) * pow(pow(n, 2.0) - 1.0, 2.0) * (6.0 + 3.0 * pn)) / (3.0 * N * pow(lambda, vec3(4.0)) * (6.0 - 7.0 * pn));',
+        '}',
 
-    // see http://blenderartists.org/forum/showthread.php?321110-Shaders-and-Skybox-madness
-    // A simplied version of the total Rayleigh scattering to works on browsers that use ANGLE
-    'vec3 simplifiedRayleigh()',
-    '{',
-    'return 0.0005 / vec3(94, 40, 18);',
-    '}',
+        // see http://blenderartists.org/forum/showthread.php?321110-Shaders-and-Skybox-madness
+        // A simplied version of the total Rayleigh scattering to works on browsers that use ANGLE
+        'vec3 simplifiedRayleigh()',
+        '{',
+        'return 0.0005 / vec3(94, 40, 18);',
+        '}',
 
-    'float rayleighPhase(float cosTheta)',
-    '{   ',
-    'return (3.0 / (16.0*pi)) * (1.0 + pow(cosTheta, 2.0));',
-    '}',
+        'float rayleighPhase(float cosTheta)',
+        '{   ',
+        'return (3.0 / (16.0*pi)) * (1.0 + pow(cosTheta, 2.0));',
+        '}',
 
-    'vec3 totalMie(vec3 lambda, vec3 K, float T)',
-    '{',
-    'float c = (0.2 * T ) * 10E-18;',
-    'return 0.434 * c * pi * pow((2.0 * pi) / lambda, vec3(v - 2.0)) * K;',
-    '}',
+        'vec3 totalMie(vec3 lambda, vec3 K, float T)',
+        '{',
+        'float c = (0.2 * T ) * 10E-18;',
+        'return 0.434 * c * pi * pow((2.0 * pi) / lambda, vec3(v - 2.0)) * K;',
+        '}',
 
-    'float hgPhase(float cosTheta, float g)',
-    '{',
-    'return (1.0 / (4.0*pi)) * ((1.0 - pow(g, 2.0)) / pow(1.0 - 2.0*g*cosTheta + pow(g, 2.0), 1.5));',
-    '}',
+        'float hgPhase(float cosTheta, float g)',
+        '{',
+        'return (1.0 / (4.0*pi)) * ((1.0 - pow(g, 2.0)) / pow(1.0 - 2.0*g*cosTheta + pow(g, 2.0), 1.5));',
+        '}',
 
-    'float sunIntensity(float zenithAngleCos)',
-    '{',
-    'return EE * max(0.0, 1.0 - exp(-((cutoffAngle - acos(zenithAngleCos))/steepness)));',
-    '}',
+        'float sunIntensity(float zenithAngleCos)',
+        '{',
+        'return EE * max(0.0, 1.0 - exp(-((cutoffAngle - acos(zenithAngleCos))/steepness)));',
+        '}',
 
-    '// Filmic ToneMapping http://filmicgames.com/archives/75',
-    'float A = 0.15;',
-    'float B = 0.50;',
-    'float C = 0.10;',
-    'float D = 0.20;',
-    'float E = 0.02;',
-    'float F = 0.30;',
-    'float W = 1000.0;',
+        '// Filmic ToneMapping http://filmicgames.com/archives/75',
+        'float A = 0.15;',
+        'float B = 0.50;',
+        'float C = 0.10;',
+        'float D = 0.20;',
+        'float E = 0.02;',
+        'float F = 0.30;',
+        'float W = 1000.0;',
 
-    'vec3 Uncharted2Tonemap(vec3 x)',
-    '{',
-    'return ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F;',
-    '}',
+        'vec3 Uncharted2Tonemap(vec3 x)',
+        '{',
+        'return ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F;',
+        '}',
 
-    'void main() ',
-    '{',
-    'float sunfade = 1.0-clamp(1.0-exp((sunPosition.y/450000.0)),0.0,1.0);',
+        'void main() ',
+        '{',
+        'float sunfade = 1.0-clamp(1.0-exp((sunPosition.y/450000.0)),0.0,1.0);',
 
-    'float reileighCoefficient = reileigh - (1.0* (1.0-sunfade));',
+        'float reileighCoefficient = reileigh - (1.0* (1.0-sunfade));',
 
-    'vec3 sunDirection = normalize(sunPosition);',
+        'vec3 sunDirection = normalize(sunPosition);',
 
-    'float sunE = sunIntensity(dot(sunDirection, up));',
+        'float sunE = sunIntensity(dot(sunDirection, up));',
 
-    // extinction (absorbtion + out scattering)
-    // rayleigh coefficients
+        // extinction (absorbtion + out scattering)
+        // rayleigh coefficients
 
-    'vec3 betaR = simplifiedRayleigh() * reileighCoefficient;',
+        'vec3 betaR = simplifiedRayleigh() * reileighCoefficient;',
 
-    // mie coefficients
-    'vec3 betaM = totalMie(lambda, K, turbidity) * mieCoefficient;',
+        // mie coefficients
+        'vec3 betaM = totalMie(lambda, K, turbidity) * mieCoefficient;',
 
-    // optical length
-    // cutoff angle at 90 to avoid singularity in next formula.
-    'float zenithAngle = acos(max(0.0, dot(up, normalize(vWorldPosition - cameraPos))));',
-    'float sR = rayleighZenithLength / (cos(zenithAngle) + 0.15 * pow(93.885 - ((zenithAngle * 180.0) / pi), -1.253));',
-    'float sM = mieZenithLength / (cos(zenithAngle) + 0.15 * pow(93.885 - ((zenithAngle * 180.0) / pi), -1.253));',
+        // optical length
+        // cutoff angle at 90 to avoid singularity in next formula.
+        'float zenithAngle = acos(max(0.0, dot(up, normalize(vWorldPosition - cameraPos))));',
+        'float sR = rayleighZenithLength / (cos(zenithAngle) + 0.15 * pow(93.885 - ((zenithAngle * 180.0) / pi), -1.253));',
+        'float sM = mieZenithLength / (cos(zenithAngle) + 0.15 * pow(93.885 - ((zenithAngle * 180.0) / pi), -1.253));',
 
-    // combined extinction factor
-    'vec3 Fex = exp(-(betaR * sR + betaM * sM));',
+        // combined extinction factor
+        'vec3 Fex = exp(-(betaR * sR + betaM * sM));',
 
-    // in scattering
-    'float cosTheta = dot(normalize(vWorldPosition - cameraPos), sunDirection);',
+        // in scattering
+        'float cosTheta = dot(normalize(vWorldPosition - cameraPos), sunDirection);',
 
-    'float rPhase = rayleighPhase(cosTheta*0.5+0.5);',
-    'vec3 betaRTheta = betaR * rPhase;',
+        'float rPhase = rayleighPhase(cosTheta*0.5+0.5);',
+        'vec3 betaRTheta = betaR * rPhase;',
 
-    'float mPhase = hgPhase(cosTheta, mieDirectionalG);',
-    'vec3 betaMTheta = betaM * mPhase;',
+        'float mPhase = hgPhase(cosTheta, mieDirectionalG);',
+        'vec3 betaMTheta = betaM * mPhase;',
 
-    'vec3 Lin = pow(sunE * ((betaRTheta + betaMTheta) / (betaR + betaM)) * (1.0 - Fex),vec3(1.5));',
-    'Lin *= mix(vec3(1.0),pow(sunE * ((betaRTheta + betaMTheta) / (betaR + betaM)) * Fex,vec3(1.0/2.0)),clamp(pow(1.0-dot(up, sunDirection),5.0),0.0,1.0));',
+        'vec3 Lin = pow(sunE * ((betaRTheta + betaMTheta) / (betaR + betaM)) * (1.0 - Fex),vec3(1.5));',
+        'Lin *= mix(vec3(1.0),pow(sunE * ((betaRTheta + betaMTheta) / (betaR + betaM)) * Fex,vec3(1.0/2.0)),clamp(pow(1.0-dot(up, sunDirection),5.0),0.0,1.0));',
 
-    //nightsky
-    'vec3 direction = normalize(vWorldPosition - cameraPos);',
-    'float theta = acos(direction.y); // elevation --> y-axis, [-pi/2, pi/2]',
-    'float phi = atan(direction.z, direction.x); // azimuth --> x-axis [-pi/2, pi/2]',
-    'vec2 uv = vec2(phi, theta) / vec2(2.0*pi, pi) + vec2(0.5, 0.0);',
-    // vec3 L0 = texture2D(skySampler, uv).rgb+0.1 * Fex;
-    'vec3 L0 = vec3(0.1) * Fex;',
+        //nightsky
+        'vec3 direction = normalize(vWorldPosition - cameraPos);',
+        'float theta = acos(direction.y); // elevation --> y-axis, [-pi/2, pi/2]',
+        'float phi = atan(direction.z, direction.x); // azimuth --> x-axis [-pi/2, pi/2]',
+        'vec2 uv = vec2(phi, theta) / vec2(2.0*pi, pi) + vec2(0.5, 0.0);',
+        // vec3 L0 = texture2D(skySampler, uv).rgb+0.1 * Fex;
+        'vec3 L0 = vec3(0.1) * Fex;',
 
-    // composition + solar disc
-    'float sundisk = smoothstep(sunAngularDiameterCos,sunAngularDiameterCos+0.00002,cosTheta);',
-    'L0 += (sunE * 19000.0 * Fex)*sundisk;',
+        // composition + solar disc
+        'float sundisk = smoothstep(sunAngularDiameterCos,sunAngularDiameterCos+0.00002,cosTheta);',
+        'L0 += (sunE * 19000.0 * Fex)*sundisk;',
 
-    'vec3 whiteScale = 1.0/Uncharted2Tonemap(vec3(W));',
+        'vec3 whiteScale = 1.0/Uncharted2Tonemap(vec3(W));',
 
-    'vec3 texColor = (Lin+L0);   ',
-    'texColor *= 0.04 ;',
-    'texColor += vec3(0.0,0.001,0.0025)*0.3;',
+        'vec3 texColor = (Lin+L0);   ',
+        'texColor *= 0.04 ;',
+        'texColor += vec3(0.0,0.001,0.0025)*0.3;',
 
-    'float g_fMaxLuminance = 1.0;',
-    'float fLumScaled = 0.1 / luminance;     ',
-    'float fLumCompressed = (fLumScaled * (1.0 + (fLumScaled / (g_fMaxLuminance * g_fMaxLuminance)))) / (1.0 + fLumScaled); ',
+        'float g_fMaxLuminance = 1.0;',
+        'float fLumScaled = 0.1 / luminance;     ',
+        'float fLumCompressed = (fLumScaled * (1.0 + (fLumScaled / (g_fMaxLuminance * g_fMaxLuminance)))) / (1.0 + fLumScaled); ',
 
-    'float ExposureBias = fLumCompressed;',
+        'float ExposureBias = fLumCompressed;',
 
-    'vec3 curr = Uncharted2Tonemap((log2(2.0/pow(luminance,4.0)))*texColor);',
-    'vec3 color = curr*whiteScale;',
+        'vec3 curr = Uncharted2Tonemap((log2(2.0/pow(luminance,4.0)))*texColor);',
+        'vec3 color = curr*whiteScale;',
 
-    'vec3 retColor = pow(color,vec3(1.0/(1.2+(1.2*sunfade))));',
+        'vec3 retColor = pow(color,vec3(1.0/(1.2+(1.2*sunfade))));',
 
-    'gl_FragColor.rgb = retColor;',
+        'gl_FragColor.rgb = retColor;',
 
-    'gl_FragColor.a = 1.0;',
-    '}'
-  ].join('\n')
+        'gl_FragColor.a = 1.0;',
+        '}'
+    ].join('\n')
 });
 
 
@@ -1372,24 +1367,24 @@ AFRAME.registerShader('gradientshader', {
     },
 
     vertexShader: [
-    'varying vec3 vWorldPosition;',
-    'void main() {',
-    ' vec4 worldPosition = modelMatrix * vec4( position, 1.0 );',
-    ' vWorldPosition = worldPosition.xyz;',
-    ' gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0 );',
-    '}'
-  ].join('\n'),
+        'varying vec3 vWorldPosition;',
+        'void main() {',
+        ' vec4 worldPosition = modelMatrix * vec4( position, 1.0 );',
+        ' vWorldPosition = worldPosition.xyz;',
+        ' gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0 );',
+        '}'
+    ].join('\n'),
 
     fragmentShader: [
-    'uniform vec3 bottomColor;',
-    'uniform vec3 topColor;',
-    'uniform float offset;',
-    'varying vec3 vWorldPosition;',
-    'void main() {',
-    ' float h = normalize( vWorldPosition ).y;',
-    ' gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( max(h, 0.0 ), 0.8 ), 0.0 ) ), 1.0 );',
-    '}'
-  ].join('\n')
+        'uniform vec3 bottomColor;',
+        'uniform vec3 topColor;',
+        'uniform float offset;',
+        'varying vec3 vWorldPosition;',
+        'void main() {',
+        ' float h = normalize( vWorldPosition ).y;',
+        ' gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( max(h, 0.0 ), 0.8 ), 0.0 ) ), 1.0 );',
+        '}'
+    ].join('\n')
 });
 
 // perlin noise generator
